f14acabe5c3f963ebe1761d9f6d05b971626c229
Hajixhayjhay” <akandehajarat@gmail.com>
2025-03-16 12:52:50 +0800

    edited files

--- vpc/output.tf
+++ vpc/output.tf
@@ -15,42 +15,42 @@ output "environment" {
 
 # export the vpc id
 output "vpc_id" {
-  value = var.aws_vpc.vpc.id
+  value = aws_vpc.vpc.id
 }
 
 # export the internet gateway
 output "internet_gateway" {
-  value = var.aws_internet_gateway.internet_gateway.id
+  value = aws_internet_gateway.internet_gateway.id
 }
 
 # export the public subnet az1 id
 output "public_subnet_az1_id" {
-  value = var.aws_subnet.public_subnet_az1.id
+  value = aws_subnet.public_subnet_az1.id
 }
 
 # export the public subnet az2 id
 output "public_subnet_az2_id" {
-  value = var.aws_subnet.public_subnet_az2.id
+  value = aws_subnet.public_subnet_az2.id
 }
 
 # export the private app subnet az1 id
 output "private_app_subnet_az1_id" {
-  value = var.aws_subnet.private_app_subnet_az1.id
+  value = aws_subnet.private_app_subnet_az1.id
 }
 
 # export the private app subnet az2 id
 output "private_app_subnet_az2_id" {
-  value = var.aws_subnet.private_app_subnet_az2.id
+  value = aws_subnet.private_app_subnet_az2.id
 }
 
 # export the private data subnet az1 id
 output "private_data_subnet_az1_id" {
-  value = var.aws_subnet.private_data_subnet_az1.id
+  value = aws_subnet.private_data_subnet_az1.id
 }
 
 # export the private data subnet az2 id
 output "private_data_subnet_az2_id" {
-  value = var.aws_subnet.private_data_subnet_az2.id
+  value = aws_subnet.private_data_subnet_az2.id
 }
 
 # export the first availability zone
