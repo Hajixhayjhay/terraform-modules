f91e71532e51abee248f761d5a6413dfc0fc8f06
Hajixhayjhay” <akandehajarat@gmail.com>
2025-03-18 12:41:20 +0800

    iam modules

--- iam_role/main.tf
+++ iam_role/main.tf
@@ -46,16 +46,16 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
   }
 }
 
-# create iam policy
+# Create IAM Policy
 resource "aws_iam_policy" "ecs_task_execution_policy" {
   name   = "${var.project_name}-${var.environment}-ecs-task-execution-role-policy"
-  policy = data.aws_iam_policy_document.assume_role_policy_document.json
+  policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json 
 }
 
-# create an iam role
+# Create an IAM Role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               ="${var.project_name}-${var.environment}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json  
 }
 
 # attach ecs task execution policy to the iam role
